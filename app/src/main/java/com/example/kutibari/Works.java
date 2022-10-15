941ba999b77c78f8bc26bda9b1419c665e25c302
Fairuz Ahmed <93150706+Tuna543@users.noreply.github.com>
2022-10-15 23:33:03 +0600

    Update Works.java

--- app/src/main/java/com/example/kutibari/Works.java
+++ app/src/main/java/com/example/kutibari/Works.java
@@ -10,6 +10,6 @@ public class Works extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_works);
-        hello
+       
     }
 }
